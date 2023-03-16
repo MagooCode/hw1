e44f06c749164e14720ca900b05c635342a5fa4c
MagooCode <125905039+MagooCode@users.noreply.github.com>
2023-03-16 17:39:36 +0400

    Add files via upload

---
+++ 16.03.2023 hw/Program.cs
@@ -0,0 +1,17 @@
+﻿using Coree.Entities;
+using Utilss.Exeptions;
+
+User User= new User("magoo_everywhere@mail.ru", "Salam123");
+
+try
+{
+	Console.WriteLine(User.PasswordChecker("salam1245"));
+}
+catch (IncorrectPasswordException ex)
+{ 
+	Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
